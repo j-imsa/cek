e9f50d72f436cc8b6bf8eb0e18f13c85b12af7dd
jimsa <cse.isalehi@gmail.com>
2023-07-20 20:39:39 +0330

    work on auth-steps

--- src/main/java/ir/jimsa/cek/config/security/CekAuthenticationProvider.java
+++ src/main/java/ir/jimsa/cek/config/security/CekAuthenticationProvider.java
@@ -2,6 +2,7 @@ package ir.jimsa.cek.config.security;
 
 import ir.jimsa.cek.model.entity.UserEntity;
 import ir.jimsa.cek.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -11,6 +12,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,33 +22,45 @@ public class CekAuthenticationProvider implements AuthenticationProvider {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final HttpServletRequest request;
 
-    public CekAuthenticationProvider(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public CekAuthenticationProvider(UserRepository userRepository, PasswordEncoder passwordEncoder, HttpServletRequest request) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.request = request;
     }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String username = authentication.getName();
-        String rawPassword = authentication.getCredentials().toString();
 
+        // ip location check
+        // ...
+        WebAuthenticationDetails details = (WebAuthenticationDetails) authentication.getDetails();
+        String ip1 = details.getRemoteAddress();
+        String ip2 = request.getRemoteAddr();
+
+        // Login from new location
+        // ...
+
+        // username and password check
+        String username = authentication.getName();
         UserEntity userEntity = userRepository.findByUsername(username);
         if (userEntity == null) {
             throw new BadCredentialsException("Can not find any user");
         }
+        String rawPassword = authentication.getCredentials().toString();
         if (!passwordEncoder.matches(rawPassword, userEntity.getEncodedPassword())) {
             throw new BadCredentialsException("Invalid password!");
         }
 
+        // Authenticated:
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(userEntity.getRole()));
-
         return new UsernamePasswordAuthenticationToken(username, passwordEncoder, authorities);
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
     }
 }
