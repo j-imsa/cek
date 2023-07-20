92602e0acd4eff617426774b4d3e0bf143c72a73
Iman Salehi <cse.isalehi@gmail.com>
2023-07-20 16:01:07 +0330

    add security config

---
+++ src/main/java/ir/jimsa/cek/config/security/ProjectSecurityConfig.java
@@ -0,0 +1,23 @@
+package ir.jimsa.cek.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(req -> req.anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
